988c0c9e17dd4ba0f01b08f4d0bfe80e37c04145
matheus341 <37388593+matheus341@users.noreply.github.com>
2022-03-25 18:42:52 -0300

    outro commit
    quase pronto, faltando somente o download da base de dados

--- teste/pages/urls.py
+++ teste/pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import UsuarioCreate
 from .import views
-
+from .views import List
 app_name = "pages"
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
     path("registrar/", UsuarioCreate.as_view(), name="registrar" ),
-
+    path("listar/", List.as_view(), name="listar" ),
 ]
--- teste/pages/views.py
+++ teste/pages/views.py
@@ -3,6 +3,9 @@ from django.views.generic import CreateView
 from django.contrib.auth.models import User
 from .forms import UserForm
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+
+
 class HomePageView(TemplateView):
     template_name = "home.html"
 
@@ -10,3 +13,7 @@ class UsuarioCreate(CreateView):
    success_url = reverse_lazy("account_login")
    template_name = "account/signup.html"
    form_class = UserForm
+
+class List(ListView):
+    model = User
+    template_name = "listagem.html"
